5b8f440191f85782f871bf02c5a7803cb5f17376
adkrueger <adkrueger@wpi.edu>
2020-03-13 15:06:07 -0400

    finished state changing portion; added notes on conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,14 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  /*  conditional rendering
+    renderTags() {
+      if(this.state.tags.length === 0) return <p>There are no tags.</p>;
+
+      return <ul>{this.state.tags.map(tag => (<li key={tag}>{tag}</li>))}</ul>;
+    }
+  */
+
   /*
   constructor() {
     super();
@@ -22,16 +30,20 @@ class Counter extends Component {
     this achieves the same thing more elegantly.
     arrow functions don't rebind the this keyword; they inherit it
   */
-  handleIncrement = () => {
-    console.log("Increment clicked", this.state.count);
+  handleIncrement = product => {
+    //console.log("Increment clicked", this.state.count);
+    // this.state.count++;   alone will update state, but won't be reflected on the view
+    // instead, do this (need to tell react what about the state is being updated):
+    console.log(product);
+    this.setState({ count: this.state.count + 1 }); // creates async call to render()
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -41,7 +53,7 @@ class Counter extends Component {
             <li key={tag}>{tag}</li> // need key to give unique li's (within this ul)
           ))}
         </ul>
-      </React.Fragment>
+      </div>
     );
   }
 
