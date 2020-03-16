ba637f6b45d48ad09e9a19aea81a5ed3c696d25d
adkrueger <adkrueger@wpi.edu>
2020-03-15 22:21:13 -0400

    navbar counts counters; App is now root

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import "./App.css";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    /* ... is the "spread" operator
+        it essentially gives us everything in whatever array follows it
+            (regardless of the length of the array)
+    */
+    const counters = [...this.state.counters];
+
+    // here we are cloning the given counter object, which we find in the counters array
+    // this way we don't directly change state
+    //    (this has to do with changes being overwritten and "how synchronous" setState is)
+    const index = counters.indexOf(counter);
+    console.log({ ...counter });
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -7,61 +7,21 @@ import Counter from "./counter";
     all with different (isolated) states.
 */
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 3 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleIncrement = counter => {
-    /* ... is the "spread" operator
-        it essentially gives us everything in whatever array follows it
-            (regardless of the length of the array)
-    */
-    const counters = [...this.state.counters];
-
-    // here we are cloning the given counter object, which we find in the counters array
-    // this way we don't directly change state
-    //    (this has to do with changes being overwritten and "how synchronous" setState is)
-    const index = counters.indexOf(counter);
-    console.log({ ...counter });
-    counters[index] = { ...counter };
-    counters[index].value++;
-    console.log(this.state);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             // we pass key and id because internally, react needs the key
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
             //value={counter.value}
             //id={counter.id}
